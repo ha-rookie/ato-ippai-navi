6e5aca77d3d29bf646b324041d68f8e8fc1aa7fc
ha-rookie <104521513+ha-rookie@users.noreply.github.com>
2026-09-02 04:17:53 +0900

    feat: add official Sakae late-night timetable data

---
+++ src/data/sakae-higashiyama-late.js
@@ -0,0 +1,39 @@
+export const SAKAE_HIGASHIYAMA_LATE = {
+  source: {
+    publisher: "名古屋市交通局",
+    license: "CC BY 4.0",
+    revision: "2025-03-29",
+    dataset: "地下鉄駅別ポケット時刻表",
+    line: "東山線"
+  },
+  origin: {
+    code: "H10",
+    name: "栄"
+  },
+  destination: {
+    code: "H22",
+    name: "藤が丘"
+  },
+  schedules: {
+    weekday: [
+      { time: "23:04", serviceMinutes: 1384, destination: "藤が丘" },
+      { time: "23:13", serviceMinutes: 1393, destination: "藤が丘" },
+      { time: "23:22", serviceMinutes: 1402, destination: "藤が丘" },
+      { time: "23:32", serviceMinutes: 1412, destination: "藤が丘" },
+      { time: "23:42", serviceMinutes: 1422, destination: "藤が丘" },
+      { time: "23:52", serviceMinutes: 1432, destination: "藤が丘" },
+      { time: "00:02", serviceMinutes: 1442, destination: "藤が丘" },
+      { time: "00:16", serviceMinutes: 1456, destination: "星ヶ丘" }
+    ],
+    saturday_holiday: [
+      { time: "23:02", serviceMinutes: 1382, destination: "藤が丘" },
+      { time: "23:12", serviceMinutes: 1392, destination: "藤が丘" },
+      { time: "23:22", serviceMinutes: 1402, destination: "藤が丘" },
+      { time: "23:32", serviceMinutes: 1412, destination: "藤が丘" },
+      { time: "23:42", serviceMinutes: 1422, destination: "藤が丘" },
+      { time: "23:52", serviceMinutes: 1432, destination: "藤が丘" },
+      { time: "00:02", serviceMinutes: 1442, destination: "藤が丘" },
+      { time: "00:16", serviceMinutes: 1456, destination: "星ヶ丘" }
+    ]
+  }
+};
